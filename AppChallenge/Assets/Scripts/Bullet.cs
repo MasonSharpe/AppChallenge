b306e359b070b23bb39a1326d375634d63d4bce2
Mason <115174711+MasonSharpe@users.noreply.github.com>
2023-09-05 13:29:51 -0700

    moreee gameplayyy

--- AppChallenge/Assets/Scripts/Bullet.cs
+++ AppChallenge/Assets/Scripts/Bullet.cs
@@ -8,12 +8,20 @@ public class Bullet : MonoBehaviour
     public float currentStoredDamage;
     public Rigidbody2D rb;
 
+
+    private void Start()
+    {
+        Destroy(gameObject, 10);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //sweetspot
         if (collision.gameObject.layer == 9)
         {
             currentStoredDamage *= 2;
             rb.velocity *= 1.3f;
+            print("ya!");
         }
     }
 
