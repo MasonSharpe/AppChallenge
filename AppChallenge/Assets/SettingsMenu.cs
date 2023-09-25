5f86331c0e987ceb7895a026dc7fd9e2eac4fe2d
Kyan0ver9000 <115186321+Kyan0ver9000@users.noreply.github.com>
2023-09-25 13:25:36 -0700

    done with the rough edges of the "settings menu" game object. now trying to work on making it look better

--- AppChallenge/Assets/SettingsMenu.cs
+++ AppChallenge/Assets/SettingsMenu.cs
@@ -1,11 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    public void SetVolume (float volume)
+    public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+    void Start()
     {
-        Debug.Log(volume);
+        resolutions= Screen.resolutions; 
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetResolution (int resulationIndex)
+    {
+        Resolution resolution = resolutions[resulationIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
